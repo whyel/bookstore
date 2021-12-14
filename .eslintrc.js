08980c9f091e98f633b89732aeeef21848ca80bd
whyel <whyel@163.com>
2021-12-14 16:37:34 +0800

    commit .*

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,7 @@ module.exports = {
     ecmaVersion: 2020
   },
   rules: {
+    indent: ['off', 2],
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
   },
